d83b21458f1f9d160668f71b030ade4dbe40c290
Domjan <domjangrkinic@gmail.com>
2022-09-08 23:47:26 +0200

    finish nav bar

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ const navSlide = () => {
         link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
       }
     });
+      burger.classList.toggle('toggle');
+
+
+
   });
 }
 
